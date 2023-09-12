d5a4ab154a25dd0fdf3842f5b65d1230a9324be5
MaximusF <mvalk@student.codam.nl>
2023-09-12 14:34:37 +0200

    executor_start

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,28 +1,23 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdarg.h>
-# include <stddef.h>
-# include <stdbool.h>
-# include <unistd.h>
-# include <signal.h>
-# include <string.h>
-# include <fcntl.h>
-# include <sys/wait.h>
-# include <sys/errno.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+
+#include "minishell.h"
 
 typedef struct s_info
 {
 	char			**command;
-	char			*heredoc;
 	char			**append_out_file;
 	char			**redirect_out_file;
+	char			*heredoc;
 	char			**redirect_in_file;
-	// t_tag			tag;
 	struct s_info	*next;
 	struct s_info	*prev;
 }	t_info;
+
+typedef	struct s_file_node
+{
+	char 		*file_name;
+	t_tag 		type;
+	t_file_node *next;
+}	t_file_node;
 
 size_t	count_command(t_info *infos)
 {
@@ -30,7 +25,7 @@ size_t	count_command(t_info *infos)
 	return (3);
 }
 
-void	execute_command(t_info *infos)
+void	execute_command_single(t_info *infos, char *envp[], int fd_in, int fd_out)
 {
 	pid_t	pid;
 
@@ -39,28 +34,71 @@ void	execute_command(t_info *infos)
 		exit(EXIT_FAILURE);
 	if (pid == 0)
 	{
-		if (infos->heredoc != NULL)	
-			handle_heredoc();
+		if (fd_in != STDIN_FILENO)
+		{
+			if (dup2(fd_in, STDIN_FILENO) < 0)
+				error_exit("dup2", errno);
+			close(fd_in);
+		}		
+		if (fd_out != STDOUT_FILENO)
+		{
+			if (dup2(fd_out, STDOUT_FILENO) < 0)
+				error_exit("dup2", errno);
+			close(fd_out);
+		}
+		if (execve(infos->command[0], infos->command, envp) < 0)
+			error_exit("execve", errno);
+		ft_putstr_fd(infos->command[0], 2);
+		ft_putendl_fd(": command not found", 2);
+		exit(127);
+	}
+	else
+		waitpid(pid, NULL, 0);
+}
+
+void	set_start_fd(int *fd_in, int *fd_out, t_info *info)
+{
+	if (info->heredoc != NULL)
+		handle_heredoc();
+	if (info->redirect_in_file != NULL)
+	{
+		while (*info->redirect_in_file != NULL)
+		{
+			fd_in = open(info->redirect_in_file, O_RDONLY);
+			if (fd_in == -1)
+				perror("infile not open");
+			info->redirect_in_file++;
+		}
+	}
+	if (info->redirect_out_file != NULL)
+	{
+		while (*info->redirect_out_file != NULL)
+		{
+			fd_in = open(info->redirect_out_file, O_CREAT | O_RDWR | O_TRUNC, 0644);
+			fd_in = open(info->redirect_out_file, O_CREAT | O_RDWR | O_APPEND, 0644);
+			if (fd_in == -1)
+				perror("outfile not open");
+			info->redirect_out_file++;
+		}
 	}
 }
 
 void	exec_loop(t_info *info)
 {
-	size_t	command_count;
-	size_t	i;
 	int		pipe_fd[2];
+	int		fd_in;
+	int		fd_out;
 
-	command_count = count_command(info);
-	// fd_in = STDIN_FILENO;
-	// fd_out = -1;
-	if (pipe(pipe_fd) == -1)
-		exit(EXIT_FAILURE);
-	while (i < command_count)
+	fd_in = STDIN_FILENO;
+	fd_out = STDOUT_FILENO;
+	while (info)
 	{
-		if (i == command_count - 1)
-			fd_out = STDOUT_FILENO;
-		else
-			fd_out = -1;
+		set_start_fd(&fd_in, &fd_out, info);
 	}
 }
 
+int main()
+{
+	t_info a = calloc(1, sizeof(t_info));
+	t_info
+}
